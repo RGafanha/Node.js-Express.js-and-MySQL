e6d70b562e71466dcddb8406cd876e57f1139c4e
ws-prg <pablo.gafanha@wilsonsons.com.br>
2021-12-29 10:16:53 -0300

    adicionado bd

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ const env = process.env;
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
     host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || '',
-    password: env.DB_PASSWORD || '',
-    database: env.DB_NAME || '',
+    user: env.DB_USER || 'ragnarok',
+    password: env.DB_PASSWORD || 'ragnarok123',
+    database: env.DB_NAME || 'banktest',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
